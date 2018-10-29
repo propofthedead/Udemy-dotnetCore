f89f6c4b8c482226d0c38df290410bae1c68436e
Foster Stule <camodude5@yahoo.com>
2018-10-29 16:01:57 -0400

    play around with dotnet run and watch commands

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -15,7 +15,8 @@ namespace DatingApp.Api.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            
+            return new string[] { "value1", "value4" };
         }
 
         // GET api/values/5
