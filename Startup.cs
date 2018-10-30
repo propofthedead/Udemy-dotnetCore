928031f690f346b1e1fc6d579d85b9820424305e
Foster Stule <camodude5@yahoo.com>
2018-10-30 10:22:06 -0400

    added a connection string

--- Startup.cs
+++ Startup.cs
@@ -25,6 +25,7 @@ namespace DatingApp.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DataContext>(x => x.UseSqlite("ConnectionString"));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
