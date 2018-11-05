19d4db96f6b531f907a90fe3abaffab9261372a9
Foster Stule <camodude5@yahoo.com>
2018-11-05 10:27:38 -0500

    did some minor modifcations to use dto

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,5 +1,6 @@
 using System.Threading.Tasks;
 using DatingApp.Api.Data;
+using DatingApp.Api.Dtos;
 using DatingApp.Api.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -16,19 +17,19 @@ namespace DatingApp.Api.Controllers
         }
 
         [HttpPost("register")]
-        public async Task<IActionResult> Register(string username, string password){
+        public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto ){
 
-            username = username.ToLower();
+            userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
 
-            if(await _repo.UserExists(username))
+            if(await _repo.UserExists(userForRegisterDto.Username))
                 return BadRequest("Username already exits");
 
             var userToCreate = new User
             {
-                Username=username
+                Username=userForRegisterDto.Username
             };
 
-            var createdUser = await _repo.Register(userToCreate,password);
+            var createdUser = await _repo.Register(userToCreate,userForRegisterDto.Password);
 
             return StatusCode(201); 
         }
